4080bb6afea6e067bbbcca2a60d0213511d6743c
Olivia Tran <oliviatran99@gmail.com>
2023-11-28 23:50:11 -0800

    revise the script

--- next_global_entry.py
+++ next_global_entry.py
@@ -29,30 +29,51 @@ def send_start_message():
 def check_openings():
     """Check for interview openings."""
     log("check_openings method started")
-    now = datetime.datetime.now()
-    delta = datetime.timedelta(weeks=int(os.environ.get('LOOK_AHEAD_WEEKS')))
-    future = now + delta
-    request_url = os.environ.get('GLOBAL_ENTRY_QUERY_URL').format(
-        timestamp=future.strftime("%Y-%m-%d"))
-    result = requests.get(request_url).json()
-    cities = [o.get('city').lower() for o in result]
-
-    search_string = os.environ.get('SEARCH_STRING').lower()
-    log(f"search_string==== {search_string}")
-    log(f"cities==== {cities}")
-    log(f"delta==== {delta}")
+    request_url = os.environ.get('GLOBAL_ENTRY_QUERY_URL')
+    response = requests.get(request_url).json()
     log(f"request_url==== {request_url}")
-    log(f"result===== {result}")
-    if search_string in cities:
-        client = Client(os.environ.get('TWILIO_ACCOUNT'),
-                        os.environ.get('TWILIO_TOKEN'))
-        message = client.messages.create(
-            to=os.environ.get('TO_NUMBER'),
-            from_=os.environ.get('TWILIO_FROM_NUMBER'),
-            body=f"Global Entry interview opportunity in {search_string} opened up just now!")
-        log("text message sent")
+    log(f"response===== {response}")
+    # Assuming response is a list of slots
+    if response:
+        for slot in response:
+            # Check if the slot is active or meets your criteria
+            if slot.get('active', False):
+                log(f"Available slot found: {slot}")
+                # Send notification message
+                client = Client(os.environ.get('TWILIO_ACCOUNT'),
+                                os.environ.get('TWILIO_TOKEN'))
+                message = client.messages.create(
+                    to=os.environ.get('TO_NUMBER'),
+                    from_=os.environ.get('TWILIO_FROM_NUMBER'),
+                    body=f"Global Entry interview slot available!")
+                log("Text message sent")
+                break
+        else:
+            log("No available slots found")
     else:
-        log("no news")
+        log("No data returned from API")
+
+    # log("check_openings method started")
+    # now = datetime.datetime.now()
+    # delta = datetime.timedelta(weeks=int(os.environ.get('LOOK_AHEAD_WEEKS')))
+    # future = now + delta
+    # request_url = os.environ.get('GLOBAL_ENTRY_QUERY_URL').format(
+    #     timestamp=future.strftime("%Y-%m-%d"))
+    # result = requests.get(request_url).json()
+    # cities = [o.get('city').lower() for o in result]
+
+    # search_string = os.environ.get('SEARCH_STRING').lower()
+
+    # if search_string in cities:
+    #     client = Client(os.environ.get('TWILIO_ACCOUNT'),
+    #                     os.environ.get('TWILIO_TOKEN'))
+    #     message = client.messages.create(
+    #         to=os.environ.get('TO_NUMBER'),
+    #         from_=os.environ.get('TWILIO_FROM_NUMBER'),
+    #         body=f"Global Entry interview opportunity in {search_string} opened up just now!")
+    #     log("text message sent")
+    # else:
+    #     log("no news")
 
 
 if __name__ == '__main__':
